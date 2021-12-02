08e411d28d8321a172bcacf0690b7317b8a27aa7
Dries <s110169@ap.be>
2021-12-02 11:53:39 +0100

    url

--- challenge-2/HttpTrigger/run.ps1
+++ challenge-2/HttpTrigger/run.ps1
@@ -13,7 +13,7 @@ param($Request, $TriggerMetadata)
 
 ## Start Making changes from here ##
 
-$url = "https://htfapi.azurewebsites.net/api/the-real-donald?teamId=29795"
+$url = "https://htfapi.azurewebsites.net/api/the-real-donald/29795"
 $method = "POST"
 $body = @{
     {
